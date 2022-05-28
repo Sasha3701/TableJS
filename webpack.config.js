16bbb31438156e17eddedd4c0ff5105674105958
Aleksandr Irhin <workirhin@yandex.ru>
2022-05-28 10:46:45 +0300

    Added default setting for webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
